32516ad5f4272642968d495d7a469749d7c2698e
Gabriel Moreno Serignollli <92124202+Serignolli@users.noreply.github.com>
2025-03-16 01:15:45 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          50: '#f5f3ff',
+          100: '#ede9fe',
+          600: '#7c3aed',
+          700: '#6d28d9',
+          900: '#4c1d95',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
